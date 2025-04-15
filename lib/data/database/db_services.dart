5d167f9435e5bdaa6d91a4497d7b5e867fca2113
Teramob Technologies <mappletechnologies@teramob-technologies-pvt-ltd.local>
2025-04-15 12:25:51 +0500

    Initial commit

---
+++ lib/data/database/db_services.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DbServices extends StatelessWidget {
+  const DbServices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
